9b9099ca57344804df12652df738c67a6c8144c9
reginaldoRodriguesdasilva <rodriguesregy664@gmail.com>
2022-09-21 20:35:54 -0300

    teste

---
+++ aula3.php
@@ -0,0 +1,18 @@
+<?php
+
+$curso = 'PHP'; //string
+$cargaHorararia = 120; // interger
+$valorCurso = 199.99; // double 
+$cargaHorarariainscricaoAberta = true; // boelean
+$conteudo = ['Variável','Função', 'POO']; // array
+
+class Aluno 
+{
+
+};
+
+$aluno = new Aluno(); // object
+
+$frequencia = null; //null
+
+echo $cargaHorararia;
